e2c8abdc1329554759b1b1f1a63869f66e67b534
Vadym Shcherbyna <vadym.shcherbyna@gmail.com>
2020-03-14 09:05:02 +0200

    Convert Indents To Spaces

--- app/Http/Controllers/auth/AuthController.php
+++ app/Http/Controllers/auth/AuthController.php
@@ -15,13 +15,13 @@ class AuthController extends Controller
      *
      * @return Illuminate\View\View|Illuminate\Support\Facades\Redirect
      */
-	public function loginForm()
+    public function loginForm()
     {
-		if (Auth::check()) {
-			return redirect(route('crud'));
-		} else {
-			return view('auth.login');
-		}
+        if (Auth::check()) {
+            return redirect(route('crud'));
+        } else {
+            return view('auth.login');
+        }
     }
 
     /**
@@ -34,54 +34,54 @@ class AuthController extends Controller
     public function loginPost(Request $request)
     {
         // validator
-		$validator = Validator::make($request->all(), [
-			'email' => 'required|string|email|max:191',
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|string|email|max:191',
             'password' => 'required|string|max:191',
             'remember' => 'boolean',
         ]);
 
-		// Return login form with  errors
-		if ($validator->fails()) {
+        // Return login form with  errors
+        if ($validator->fails()) {
             return redirect(route('login'))->withErrors($validator)->withInput();
         }
-			
-        // Check "remember" flag
-		$remember = false;
-		if ($request->input('remember')) {
-		    $remember = true;
-		}
-			
-		// Auth user  or create  new  user
-		$users = User::where('user_group_id', 1)->get();
 
-		// Create new  account with user rules
-		if ($users->count() > 0) {
+        // Check "remember" flag
+        $remember = false;
+        if ($request->input('remember')) {
+            $remember = true;
+        }
+            
+        // Auth user  or create  new  user
+        $users = User::where('user_group_id', 1)->get();
+
+        // Create new  account with user rules
+        if ($users->count() > 0) {
             // Auth admin
-			// Credentials array
+            // Credentials array
             $credentials = [
-				'email' => $request->input('email'),
-				'password' => $request->input('password'),
+                'email' => $request->input('email'),
+                'password' => $request->input('password'),
                 'user_group_id' => 1
             ];
-			
+
             // Check auth
-			if (Auth::attempt($credentials, $remember)) {
-			    return redirect(route('crud'));
-			} else {
-			    return redirect(route('login'))->withInput();
-			}
-		} else {
+            if (Auth::attempt($credentials, $remember)) {
+                return redirect(route('crud'));
+            } else {
+                return redirect(route('login'))->withInput();
+            }
+        } else {
             // Create new  account with admin rules
             $User = User::create([
-				'email' => $request->input('email'),
-				'password' => bcrypt($request->input('password')),
+                'email' => $request->input('email'),
+                'password' => bcrypt($request->input('password')),
                 'user_group_id' => 1
             ]);
 
             //  Auth new  account and  redirect  to  crud area
             Auth::login($User, true);
             return redirect(route('crud'));
-		}
+        }
     }
 
     /**
@@ -89,9 +89,9 @@ class AuthController extends Controller
      *
      * @return Illuminate\Support\Facades\Redirect
      */
-	public function logout()
+    public function logout()
     {
-		Auth::logout();
-		return redirect(route('login'));
-	}
+        Auth::logout();
+        return redirect(route('login'));
+    }
 }
